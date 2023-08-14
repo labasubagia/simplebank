45d042c2c9e036840bebc5ebfe91d30fa5a7113e
Laba Subagia <labasubagia22@gmail.com>
2023-08-14 09:20:30 +0800

    [docker] start.sh

--- start.sh
+++ start.sh
@@ -2,6 +2,10 @@
 
 set -e
 
+# Note: Enable this if you use prod .env, not in docker compose
+# echo "load env variables"
+# source /app/.env
+
 echo "run db migration"
 /app/migrate -path /app/migration -database "$DB_SOURCE" -verbose up
 
