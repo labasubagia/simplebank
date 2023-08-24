faa109fb0feee505d25f372c3360e5fbfe86c817
Laba Subagia <labasubagia22@gmail.com>
2023-08-24 23:39:15 +0800

    feat: authorization

--- gapi/rpc_update_user.go
+++ gapi/rpc_update_user.go
@@ -15,8 +15,17 @@ import (
 )
 
 func (server *Server) UpdateUser(ctx context.Context, req *pb.UpdateUserRequest) (*pb.UpdateUserResponse, error) {
+	authPayload, err := server.authorizeUser(ctx)
+	if err != nil {
+		return nil, unauthenticatedError(err)
+	}
+
 	if violations := validateUpdateUserRequest(req); violations != nil {
 		return nil, invalidArgumentError(violations)
+	}
+
+	if authPayload.Username != req.Username {
+		return nil, status.Errorf(codes.PermissionDenied, "cannot update other user's info")
 	}
 
 	arg := db.UpdateUserParams{
