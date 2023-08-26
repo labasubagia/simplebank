e147bc8f60718ed48a79c8d9ff7431a3ed085b90
Laba Subagia <labasubagia22@gmail.com>
2023-08-26 19:00:04 +0800

    feat: disable skip retry send email

--- worker/task_send_verify_email.go
+++ worker/task_send_verify_email.go
@@ -2,7 +2,6 @@ package worker
 
 import (
 	"context"
-	"database/sql"
 	"encoding/json"
 	"fmt"
 
@@ -48,9 +47,10 @@ func (processor *RedisTaskProcessor) ProcessTaskSendVerifyEmail(ctx context.Cont
 	}
 	user, err := processor.store.GetUser(ctx, payload.Username)
 	if err != nil {
-		if err == sql.ErrNoRows {
-			return fmt.Errorf("user doesn't exists: %w", asynq.SkipRetry)
-		}
+		// ? Uncomment this if you want to skip retry when user not found
+		// if err == sql.ErrNoRows {
+		// 	return fmt.Errorf("user doesn't exists: %w", asynq.SkipRetry)
+		// }
 		return fmt.Errorf("failed to get user: %w", err)
 	}
 
