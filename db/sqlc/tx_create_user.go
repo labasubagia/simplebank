cb61ac0d31c9395be6edf75e31f6e74294a9e645
Laba Subagia <labasubagia22@gmail.com>
2023-08-31 08:42:12 +0800

    fix: create user tx

--- db/sqlc/tx_create_user.go
+++ db/sqlc/tx_create_user.go
@@ -17,11 +17,11 @@ func (store *SQLStore) CreateUserTx(ctx context.Context, arg CreateUserTxParams)
 	var result CreateUserTxResult
 	err := store.execTx(ctx, func(q *Queries) error {
 		var err error
-		user, err := q.CreateUser(ctx, arg.CreateUserParams)
+		result.User, err = q.CreateUser(ctx, arg.CreateUserParams)
 		if err != nil {
 			return err
 		}
-		return arg.AfterCreate(user)
+		return arg.AfterCreate(result.User)
 	})
 	return result, err
 }
