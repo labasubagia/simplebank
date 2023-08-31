8b509895a0cde721e613bd63cb48c972902f91c8
Laba Subagia <labasubagia22@gmail.com>
2023-08-31 18:48:02 +0800

    test: grpc login

---
+++ gapi/rpc_login_user_test.go
@@ -0,0 +1,139 @@
+package gapi
+
+import (
+	"context"
+	"testing"
+
+	"github.com/jackc/pgx/v5"
+	mock_db "github.com/labasubagia/simplebank/db/mock"
+	db "github.com/labasubagia/simplebank/db/sqlc"
+	"github.com/labasubagia/simplebank/pb"
+	mock_worker "github.com/labasubagia/simplebank/worker/mock"
+	"github.com/stretchr/testify/require"
+	"go.uber.org/mock/gomock"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+func TestLoginUser(t *testing.T) {
+	user, password := randomUser(t)
+
+	testCases := []struct {
+		name          string
+		req           *pb.LoginUserRequest
+		buildStubs    func(store *mock_db.MockStore)
+		checkResponse func(t *testing.T, res *pb.LoginUserResponse, err error)
+	}{
+		{
+			name: "OK",
+			req: &pb.LoginUserRequest{
+				Username: user.Username,
+				Password: password,
+			},
+			buildStubs: func(store *mock_db.MockStore) {
+				store.EXPECT().GetUser(gomock.Any(), user.Username).
+					Times(1).
+					Return(user, nil)
+				store.EXPECT().CreateSession(gomock.Any(), gomock.Any()).
+					Times(1).
+					Return(db.Session{Username: user.Username}, nil)
+			},
+			checkResponse: func(t *testing.T, res *pb.LoginUserResponse, err error) {
+				require.NoError(t, err)
+				require.NotNil(t, res)
+			},
+		},
+		{
+			name: "ErrCreateSession",
+			req: &pb.LoginUserRequest{
+				Username: user.Username,
+				Password: password,
+			},
+			buildStubs: func(store *mock_db.MockStore) {
+				store.EXPECT().GetUser(gomock.Any(), user.Username).
+					Times(1).
+					Return(user, nil)
+				store.EXPECT().CreateSession(gomock.Any(), gomock.Any()).
+					Times(1).
+					Return(db.Session{}, pgx.ErrTxClosed)
+			},
+			checkResponse: func(t *testing.T, res *pb.LoginUserResponse, err error) {
+				require.Error(t, err)
+				st, ok := status.FromError(err)
+				require.True(t, ok)
+				require.Equal(t, codes.Internal, st.Code())
+			},
+		},
+		{
+			name: "InvalidInput",
+			req: &pb.LoginUserRequest{
+				Username: "invalid_username_?#@",
+				Password: "",
+			},
+			buildStubs: func(store *mock_db.MockStore) {
+			},
+			checkResponse: func(t *testing.T, res *pb.LoginUserResponse, err error) {
+				require.Error(t, err)
+				st, ok := status.FromError(err)
+				require.True(t, ok)
+				require.Equal(t, codes.InvalidArgument, st.Code())
+			},
+		},
+		{
+			name: "NotFound",
+			req: &pb.LoginUserRequest{
+				Username: user.Username,
+				Password: password,
+			},
+			buildStubs: func(store *mock_db.MockStore) {
+				store.EXPECT().GetUser(gomock.Any(), user.Username).
+					Times(1).
+					Return(db.User{}, db.ErrRecordNotFound)
+			},
+			checkResponse: func(t *testing.T, res *pb.LoginUserResponse, err error) {
+				require.Error(t, err)
+				st, ok := status.FromError(err)
+				require.True(t, ok)
+				require.Equal(t, codes.NotFound, st.Code())
+			},
+		},
+		{
+			name: "ErrGetUser",
+			req: &pb.LoginUserRequest{
+				Username: user.Username,
+				Password: "invalid_password",
+			},
+			buildStubs: func(store *mock_db.MockStore) {
+				store.EXPECT().GetUser(gomock.Any(), user.Username).
+					Times(1).
+					Return(db.User{}, pgx.ErrTxClosed)
+			},
+			checkResponse: func(t *testing.T, res *pb.LoginUserResponse, err error) {
+				require.Error(t, err)
+				st, ok := status.FromError(err)
+				require.True(t, ok)
+				require.Equal(t, codes.Internal, st.Code())
+			},
+		},
+	}
+
+	for i := range testCases {
+		tc := testCases[i]
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+
+			storeCtrl := gomock.NewController(t)
+			defer storeCtrl.Finish()
+			store := mock_db.NewMockStore(storeCtrl)
+
+			taskCtrl := gomock.NewController(t)
+			defer taskCtrl.Finish()
+			taskDistributor := mock_worker.NewMockTaskDistributor(taskCtrl)
+			tc.buildStubs(store)
+
+			server := newTestServer(t, store, taskDistributor)
+			res, err := server.LoginUser(context.Background(), tc.req)
+			tc.checkResponse(t, res, err)
+		})
+	}
+}
